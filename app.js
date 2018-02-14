3bbf828d54f1d824b53d980d069e152762b1683b
zras2 <zras2@mini2.local>
2018-02-14 17:46:59 -0500

    Started

---
+++ app.js
@@ -0,0 +1,17 @@
+// Sample node.js web app 
+// For demo only
+'use strict';
+
+var express = require('express'),
+    app = express();
+
+app.set('views', 'views');
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res) {
+    res.render('home', {
+  });
+});
+
+app.listen(8080);
+module.exports.getApp = app;
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Test app 
+
+This is a quick and dirty test node.js app cobbled together for the purposes of demonstrating a basic CI/CD workflow with Docker Hub ...
+
+## Instructions for use
+
+Unzip and Enjoy 
+
